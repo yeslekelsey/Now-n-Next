cc3800e5f3154b3df3817f38990f08081b835762
Kelsey McIntyre <kelsrmcintyre@gmail.com>
2023-07-17 14:11:45 -0400

    Initial Commit

---
+++ Now n' Next/Now_n__NextApp.swift
@@ -0,0 +1,17 @@
+//
+//  Now_n__NextApp.swift
+//  Now n' Next
+//
+//  Created by Kelsey McIntyre on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Now_n__NextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Now n' NextUITests/Now_n__NextUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Now_n__NextUITestsLaunchTests.swift
+//  Now n' NextUITests
+//
+//  Created by Kelsey McIntyre on 7/17/23.
+//
+
+import XCTest
+
+final class Now_n__NextUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
