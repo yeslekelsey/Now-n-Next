a91fd1d7e2d8795ffb72b16c6ffd3205f51f2f9c
Kelsey McIntyre <kelsrmcintyre@gmail.com>
2023-07-21 12:14:32 -0400

    Update HQ Question 1.swift

--- Now n' Next/Health Quiz/HQ Question 1.swift
+++ Now n' Next/Health Quiz/HQ Question 1.swift
@@ -8,26 +8,28 @@
 import SwiftUI
 
 struct HQ_Question_1: View {
-    @State private var right = "choice C"
+    @State private var right = ""
     var body: some View {
         NavigationStack {
             ZStack {
+                Color(red: 0.98039215686, green: 0.81960784313, blue: 0.57647058823)
+                    .edgesIgnoringSafeArea(.all)
                 VStack {
                     Text("Question 1: ")
                         .font(.title)
-                    Text("Insert question here...")
-                    Button("choice A") {
+                    Text("Why is mental health important?")
+                    Button("We can manage and understand our feelings") {
                         right = "choice A" }
                     .buttonStyle(BorderedProminentButtonStyle())
-                    Button("choice B") {
+                    Button("To prevent more serious problems later on") {
                         right = "choice B" }
                     .buttonStyle(BorderedProminentButtonStyle())
-                    Button("choice C") {
+                    Button("To create a better, more mindful world") {
                         right = "choice C" }
                     .buttonStyle(BorderedProminentButtonStyle())
-                    if(right == "choice A" || right == "choice B") { Text("Sorry, wrong answer! Go back and check the websites again.")}
-                    else if(right == "choice C") {
-                        Text("Right, good job!")}
+                    if(right == "choice A" || right == "choice B" || right == "choice C") {
+                        Text("Right, good job!")
+                       }
                     NavigationLink(destination: HQ_Question_2()) {
                         Text("Next Question ➡️")
                     }
